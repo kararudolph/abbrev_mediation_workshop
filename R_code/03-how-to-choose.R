e7cbf336cc7a6dc708f2758a25a255dc9fd69866
nhejazi <kara.rudolph@gmail.com>
2024-04-11 04:30:04 +0000

    nhejazi published a site update

--- R_code/03-how-to-choose.R
+++
@@ -1,7 +0,0 @@
-## ----pressure, echo=FALSE, fig.cap="", fig.ext=if (knitr:::is_latex_output()) "pdf" else "png", out.width = "100%", results="asis"----
-knitr::include_graphics(here::here("img", "ctndag.png"))
-
-
-## ---- echo=FALSE, fig.cap="", fig.ext=if (knitr:::is_latex_output()) "pdf" else "png", out.width = "75%", results="asis"----
-knitr::include_graphics(here::here("img", "tmleesttotal.png"))
-
